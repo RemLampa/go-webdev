0e59e4039e021b96eec52d7d43dfadad6e9dc5ca
ibleedfilm <rem@lansangan.com>
2018-05-24 08:03:54 +0800

    Add solution to template-07 :sparkles:

---
+++ templates/07_hands-on/main.go
@@ -0,0 +1,126 @@
+package main
+
+import (
+	"html/template"
+	"log"
+	"os"
+)
+
+type menuItem struct {
+	Name  string
+	Price float64
+}
+
+type menu struct {
+	Name  string
+	Items []menuItem
+}
+
+type restaurant struct {
+	Name     string
+	MenuList []menu
+}
+
+var tpl *template.Template
+
+func init() {
+	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
+}
+
+func main() {
+	restaurants := []restaurant{
+		restaurant{
+			Name: "Pizza Hut",
+			MenuList: []menu{
+				menu{
+					Name: "Breakfast",
+					Items: []menuItem{
+						menuItem{
+							Name:  "Bacon and Eggs",
+							Price: 50.00,
+						},
+						menuItem{
+							Name:  "Pancakes",
+							Price: 30.00,
+						},
+					},
+				},
+				menu{
+					Name: "Lunch",
+					Items: []menuItem{
+						menuItem{
+							Name:  "Hamburger",
+							Price: 75.00,
+						},
+						menuItem{
+							Name:  "Sausages",
+							Price: 50.00,
+						},
+					},
+				},
+				menu{
+					Name: "Dinner",
+					Items: []menuItem{
+						menuItem{
+							Name:  "Lasagna",
+							Price: 100.00,
+						},
+						menuItem{
+							Name:  "T-Bone Steak",
+							Price: 250.00,
+						},
+					},
+				},
+			},
+		},
+		restaurant{
+			Name: "Shakeys",
+			MenuList: []menu{
+				menu{
+					Name: "Breakfast",
+					Items: []menuItem{
+						menuItem{
+							Name:  "Bacon and Eggs",
+							Price: 50.00,
+						},
+						menuItem{
+							Name:  "Pancakes",
+							Price: 30.00,
+						},
+					},
+				},
+				menu{
+					Name: "Lunch",
+					Items: []menuItem{
+						menuItem{
+							Name:  "Hamburger",
+							Price: 75.00,
+						},
+						menuItem{
+							Name:  "Sausages",
+							Price: 50.00,
+						},
+					},
+				},
+				menu{
+					Name: "Dinner",
+					Items: []menuItem{
+						menuItem{
+							Name:  "Lasagna",
+							Price: 100.00,
+						},
+						menuItem{
+							Name:  "T-Bone Steak",
+							Price: 250.00,
+						},
+					},
+				},
+			},
+		},
+	}
+
+	err := tpl.Execute(os.Stdout, restaurants)
+	if err != nil {
+		log.Fatalln(err)
+	}
+}
