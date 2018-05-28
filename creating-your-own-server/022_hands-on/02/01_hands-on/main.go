1042e0b7e9520799e8e17b96360c9ff998c1d15f
ibleedfilm <rem@lansangan.com>
2018-05-28 23:35:35 +0800

    Add solution to create-sever 02-01 :sparkles:

---
+++ creating-your-own-server/022_hands-on/02/01_hands-on/main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"io"
+	"log"
+	"net"
+)
+
+func main() {
+	ln, err := net.Listen("tcp", ":8080")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	defer ln.Close()
+
+	for {
+		conn, err := ln.Accept()
+		if err != nil {
+			log.Fatalln(err)
+		}
+
+		io.WriteString(conn, "I see you connected.")
+
+		conn.Close()
+	}
+}
